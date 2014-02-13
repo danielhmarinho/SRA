59fc55188f0f88805f887fe50a1fb985d59ae450
thiagovsk <thiagovsk@linux-tk0n.site>
2014-02-13 14:04:32 -0200

    Fixing tests of ability and test user with student role. :qw '

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,29 +1,16 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
-
+ 
 FactoryGirl.define do
-  factory :aluno do |f|
+  factory :user do |f|
     f.name "Alessandro"
     f.username "alessandrocb"
     f.matricula "123456789"
     f.password "123456789"
     f.password_confirmation "123456789"
-    f.as_user_type "Aluno"
+    f.after(:create) {|user| user.add_role(:student)}
   end
-  factory :professor do |f|
-    f.name "Joao"
-    f.username "joaoabc"
-    f.matricula "123456789"
-    f.password "123456789"
-    f.password_confirmation "123456789"
-    f.as_user_type "Professor"
-  end
-  factory :servidor do |f|
-    f.name "Maria"
-    f.username "Mariaabc"
-    f.matricula "123456789"
-    f.password "123456789"
-    f.password_confirmation "123456789"
-    f.as_user_type "Servidor"
-  end
-
+  
+  
+ 
+  
 end
