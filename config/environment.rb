c49f71710e3033bf33b63e6d5b6d9976fb4a8a7c
Guilherme Baufaker Rêgo <gbre.111@gmail.com>
2014-02-18 23:37:10 -0300

    Removing Brazilian Rails

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
 # -*- encoding : utf-8 -*-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-require 'brcpfcnpj'
 
 # Initialize the rails application
 SRA::Application.initialize!
