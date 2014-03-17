9c467805bfe1472a01d640b68c2d50b65055f14d
Thaiane <thaiane.unb@hotmail.com>
2014-03-17 15:06:10 -0300

    Creating table and header

--- app/models/listatendimento.rb
+++ app/models/listatendimento.rb
@@ -2,7 +2,9 @@
 
   require "prawn"
   require 'prawn/layout'
+
 class Listatendimento < ActiveRecord::Base
+  
     attr_accessor :path
 
     def initialize(path = nil)
@@ -15,6 +17,8 @@ class Listatendimento < ActiveRecord::Base
         :margin      => [40, 40]
     }
 
+  
+
     def pdf
       Prawn::Document.new(PDF_OPTIONS) do |pdf|
 
@@ -25,14 +29,41 @@ class Listatendimento < ActiveRecord::Base
           pdf.text "Registros", :size => 20, :align => :center, :style => :bold
 
 
+          data = [["Coluna 1", "Coluna 2", "Coluna 3"]] 
+
+          10.times do |i|
+            data += [["linha #{i}", "linha #{i+1}", "linha #{i+1}"]]  
+          end
+
+          #@atendimentos.each do |atendimento|
+          #  data += [["linha #{i}", "linha #{i+1}", "linha #{i+1}"]]  
+          #end
+
+          pdf.table(data, :header => true)
+
+
           pdf.repeat :all do
 
+            #Header
+            pdf.bounding_box [pdf.bounds.left, pdf.bounds.top + 25], :width  => pdf.bounds.width do
+              pdf.text "Universidade de Brasília - Faculdade Gama", :align => :center
+              pdf.move_down(5)
+              pdf.stroke_horizontal_rule 
+              pdf.move_down(5)             
+             end
+
+            #Footer
             pdf.bounding_box [pdf.bounds.left, pdf.bounds.bottom + 25], :width  => pdf.bounds.width do
               pdf.stroke_horizontal_rule
               pdf.move_down(5)
-              pdf.text "Brasília , #{Time.now.strftime("%d/%m/%Y")}.", :align => :center
-              #pdf.text "#{current_user.first_name}", :size => 10
-             end
+              pdf.text "Brasília, #{Time.now.strftime("%d/%m/%Y")}.", :align => :center
+              
+              pdf.page_count.times do |i|
+                pdf.go_to_page(i+1)
+                pdf.draw_text "#{i+1} / #{pdf.page_count}", :at=>[1,1] 
+              end
+            end
+
           end
 
       end
