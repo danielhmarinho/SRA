d935192354daa94064a8d878adaa295af742490b
Thaiane <thaiane.unb@hotmail.com>
2014-03-31 10:55:57 -0300

    Creating label about atendimentos Signed by Gabriel Silva <gabriel.ssilva@outlook.com>.

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,7 @@
   # -*- encoding : utf-8 -*-
   
 class Report < ActiveRecord::Base
+  belongs_to :place
   attr_accessible :start_date, :end_date, :place
   attr_accessor :start_date, :end_date, :place
   
