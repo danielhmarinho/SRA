136f5dbfa5f6b9215f20c8f5769731f4fa5c4ee4
AlessandroCaetano <alessandro.caetanob@gmail.com>
2014-04-25 17:57:35 -0300

    Giving mass acess to role_ids on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :username, :password, :password_confirmation, :remember_me,:name, :matricula
+  attr_accessible :username, :password, :password_confirmation, :remember_me,:name, :matricula, :role_ids
 
   def email_required?
     false
