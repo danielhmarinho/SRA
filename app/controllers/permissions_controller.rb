258725bb4d5f36df3a23052a9e768cd47b905653
Carlos Henrique Ferreira Oliveira <carloshfoliveira@icloud.com>
2014-04-25 09:55:09 -0300

    Fixing routes for permissions, user and roles filters in PermissionsController

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -1,10 +1,24 @@
+# -*- encoding: utf-8 -*-
 class PermissionsController < ApplicationController
-
-	def index
-		@user = User.all
+  authorize_resource :class => false
+  def index
+    @user = User.where(User.arel_table[:id].not_eq(current_user.id))
   end
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        redirect_as_controller(format, permission_path, notice: 'Permissões alteradas com sucesso')
+      else
+        format.html { render action: "edit" }
+
+      end
+    end
   end
 end
