7d400c7ad0cbc7bed7825b514fd1191294df3eb7
Gabriel Silva <gabriel.ssilva@outlook.com>
2014-04-28 16:12:50 -0300

    Testing the users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-
   def create
     @user = User.new(params[:user])
 
@@ -18,33 +12,12 @@ class UsersController < ApplicationController
     @user.external_user = true
     @user.encrypted_password = Digest::MD5::hexdigest params[:user][:password]
 
-    if @user.save
-      redirect_as_controller(format, root_path, notice: 'Usuário Externo criado com sucesso.')
-    else
-      render action: "new"
-    end
-  end
-
-
-  def update
-    @user = User.find(params[:id])
-
     respond_to do |format|
-      if @user.update_attributes(params[:user])
-        redirect_as_controller(format, users_path, notice: 'Usuário Externo alterado com sucesso.')
+      if @user.save
+        redirect_as_controller(format, root_path, notice: 'Usuário Externo criado com sucesso.')
       else
-        format.html { render action: "edit" }
+        format.html { render action: "new" }
       end
-    end
-  end
-
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-
-    respond_to do |format|
-      redirect_as_controller(format, users_path, notice: 'Usuário Externo removido com sucesso.')
     end
   end
 
