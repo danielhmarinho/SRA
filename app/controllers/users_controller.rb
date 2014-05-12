29cee4f610ffaf03e199456f30ce4808385fae68
Thaiane <thaiane.unb@hotmail.com>
2014-05-12 12:14:14 -0300

    Changing edit method and creating the first version of the form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
 
+   
     @user.add_role :external_user
     @user.external_user = true
     @user.encrypted_password = Digest::MD5::hexdigest params[:user][:password]
@@ -30,6 +31,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+    @user.encrypted_password = Digest::MD5::hexdigest params[:user][:password]
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
