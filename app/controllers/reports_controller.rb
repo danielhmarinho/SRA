77e02d396c7ef92c9ff6c28368175437dca6ccbd
Gabriel Silva <gabriel.ssilva@outlook.com>
2014-05-19 15:25:37 -0300

    Creating method to generate images from svg and put them on the report.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 require "prawn"
 require 'prawn/layout'
+require "RMagick"
 
 class ReportsController < ApplicationController
   load_and_authorize_resource
@@ -13,7 +14,7 @@ class ReportsController < ApplicationController
     @report = Report.new(params[:report])
 
     if @report.valid?
-      generate_data
+      save_report(params[:report],false)
 
       path = Rails.root.join("app", "assets", "relatorio.pdf")
       send_file path, :filename => "RelatorioAtendimentos.pdf", :type => "application/pdf"
@@ -26,15 +27,39 @@ class ReportsController < ApplicationController
     end
   end
 
+  def save_report_with_graph
+    
+    svg_graph1 = clean_svg_data(params[:graphs][:graph1])
+    svg_graph2 = clean_svg_data(params[:graphs][:graph2])
+    filter_attributes = params[:graphs][:attributes]
+    
+    save_graph(svg_graph1, "graph1")
+    save_graph(svg_graph2, "graph2")
+
+    save_report(filter_attributes,true)
+    
+  end
+
+
   private
 
-     def generate_data
+    def save_graph(svg, file_name)
+      img = Magick::Image::from_blob(svg)  { self.format = 'SVG'; }.first
+      img.format = 'PNG'
+      path = "#{Rails.root}/app/assets/images/#{file_name}.png"
+      img.write(path)
+    end
 
-      place = nil
+    def clean_svg_data(svg)
+      svg_graph = svg.match(/<svg(.)*<\/svg>/)[0]
+      svg_graph.gsub("'Lucida Grande', 'Lucida Sans Unicode, '", "")
+    end
+
+    def generate_data(params)
 
       atendimentos_data = [ format_titles( ["Nome", "Matrícula", "Data/Hora", "Público-alvo", "Tipo de Atendimento"] ) ]
 
-      atendimentos = filter_atendimentos params[:report]
+      atendimentos = filter_atendimentos params
 
       atendimentos.each do |atendimento|
         user = atendimento.user
@@ -44,10 +69,14 @@ class ReportsController < ApplicationController
         atendimentos_data += [[user.name, user.matricula, l(atendimento.try(:data), :format => :long), role, type ]]
       end
 
+      atendimentos_data
+    end
 
+    def save_report(params, has_image)
       path = Rails.root.join("app", "assets", "relatorio.pdf")
+      atendimentos_data = generate_data params
 
-      pdf(atendimentos_data, place).render_file(path)
+      pdf(atendimentos_data, has_image).render_file(path)
     end
 
 
@@ -58,7 +87,7 @@ class ReportsController < ApplicationController
     end
     
 
-    def pdf(atendimentos_data, place)
+    def pdf(atendimentos_data, has_image)
       
       pdf_options = {
         :page_size => "A4",
@@ -69,8 +98,18 @@ class ReportsController < ApplicationController
       Prawn::Document.new(pdf_options) do |pdf|
           
           pdf.bounding_box [pdf.bounds.left, pdf.bounds.top - 90], :width => pdf.bounds.width, :height => 440 do
+          
+            if(has_image)
+              pdf.image "#{Rails.root}/app/assets/images/graph1.png", :vposition => :center, :height => 300, :width => 900
+              pdf.start_new_page
+              pdf.image "#{Rails.root}/app/assets/images/graph2.png", :vposition => :center, :height => 230, :width => 900
+              pdf.start_new_page
+            end
+            
+
             columns = { 0 => 190, 1 => 75, 2 => 185, 3 => 100, 4 => 250}
             pdf.table(atendimentos_data, :header => true, :cell_style => { :inline_format => true, :align => :center } ,:column_widths => columns )
+          
           end
 
           pdf.page_count.times do |i|
@@ -92,10 +131,12 @@ class ReportsController < ApplicationController
           end
 
             #Footer
-            pdf.bounding_box [pdf.bounds.left, pdf.bounds.bottom + 25], :width => pdf.bounds.width do
+            pdf.bounding_box [pdf.bounds.left, pdf.bounds.bottom + 40], :width => pdf.bounds.width do
               pdf.stroke_horizontal_rule
               pdf.move_down(5)
               pdf.text "Brasília, #{Time.now.strftime("%d/%m/%Y")}", :align => :center
+              pdf.move_down(10)
+              pdf.text "#{current_user.name}: ___________________________________________", :align => :center
             end
           end
       end
