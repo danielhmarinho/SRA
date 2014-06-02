b26f472fad7cf265a6c06d9782e52ef3976ff258
thiagovsk <thiagitosouza@hotmail.com>
2014-06-02 16:00:12 -0300

    Refractoring grap.rb

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -11,13 +11,19 @@ class Graph < ActiveRecord::Base
 
   def dates_are_valid
     if self.end_date.present? && self.start_date.present?
-      start_date = DateTime.strptime(self.start_date, "%d/%m/%Y")
-      end_date = DateTime.strptime("#{self.end_date} 23:59:59", "%d/%m/%Y %H:%M:%S")
+
+      set_date
 
       if end_date < start_date
         errors.add(:end_date, "A data final deve ser após a data de início do intervalo.")
       end
     end
   end
+
+  def set_date
+    start_date = DateTime.strptime(self.start_date, "%d/%m/%Y")
+    end_date = DateTime.strptime("#{self.end_date} 23:59:59", "%d/%m/%Y %H:%M:%S")
+  end
+
 
 end
