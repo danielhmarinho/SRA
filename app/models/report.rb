faf7aa696ef21ebebdbdddc111c05b6a5441a04a
thiagovsk <thiagitosouza@hotmail.com>
2014-06-02 16:00:23 -0300

    Refractoring report.rb

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,4 @@
-  # -*- encoding : utf-8 -*-
+# -*- encoding : utf-8 -*-
 
 class Report
   include ActiveModel::Validations
@@ -19,14 +19,20 @@ class Report
   end
 
   def dates_are_valid
-  	if self.end_date.present? && self.start_date.present?
-  		start_date = DateTime.strptime(self.start_date, "%d/%m/%Y")
-      	end_date = DateTime.strptime("#{self.end_date} 23:59:59", "%d/%m/%Y %H:%M:%S")
+    if self.end_date.present? && self.start_date.present?
 
-  	 	if end_date < start_date
-  			errors.add(:end_date, "A data final deve ser após a data de início do relatório.")
-  		end
-  	end
+      set_date
+
+      if end_date < start_date
+        errors.add(:end_date, "A data final deve ser após a data de início do relatório.")
+      end
+    end
+  end
+
+  def set_date
+    start_date = DateTime.strptime(self.start_date, "%d/%m/%Y")
+    end_date = DateTime.strptime("#{self.end_date} 23:59:59", "%d/%m/%Y %H:%M:%S")
+
   end
 
   def persisted?
