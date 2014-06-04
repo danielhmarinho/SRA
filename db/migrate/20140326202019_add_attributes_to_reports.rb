8b32674ae8743faabd2355813a76e265ac3dea0a
MaxAlmeida <llewxam100@hotmail.com>
2014-06-04 10:42:37 -0300

    Correcting database.yml

--- db/migrate/20140326202019_add_attributes_to_reports.rb
+++
@@ -1,7 +0,0 @@
-class AddAttributesToReports < ActiveRecord::Migration
-  def change
-    add_column :reports, :start_date, :date
-    add_column :reports, :end_date, :date
-    add_column :reports, :place, :string
-  end
-end
