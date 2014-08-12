c50cb0136eb5544ca620b08ced02186332917192
Gabriel Silva <gabriel.ssilva@outlook.com>
2014-08-12 16:27:47 -0300

    Adding validation to block user without roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,16 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-
-    resource
     if current_user.nil?
       root_path
     elsif current_user.has_role? :administrative or  current_user.has_role? :student or current_user.has_role? :professor or current_user.has_role? :manager or current_user.has_role? :external_user or current_user.has_role? :admin
       new_atendimento_path
     else
-      atendimentos_path
+      flash.discard(:notice)
+      flash[:error] = "Não foi possível recuperar informações do usuário"
+      sign_out resource
+      root_path
     end
-
-
   end
 
   rescue_from CanCan::AccessDenied do |exception|
