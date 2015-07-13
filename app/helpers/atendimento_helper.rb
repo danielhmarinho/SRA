7fc4b52ad598dff58b32c2569d12e8fe3ce0b1c5
Matheus S. Pereira <matheussilva.fga@gmail.com>
2015-07-13 16:23:03 -0300

    Creating and Applying the method check_place in the atendimento_helper

--- app/helpers/atendimento_helper.rb
+++ app/helpers/atendimento_helper.rb
@@ -1,12 +1,10 @@
 # -*- encoding : utf-8 -*-
 module AtendimentoHelper
 
-  def check_cookie
-    if cookies[:place_name]
-      place_selected =  Place.where(active: true, name: cookies[:place_name])
-    else
-      place_selected = nil
-    end
+  def check_place
+  	place_name = place_client("none")
+    place_selected =  Place.where(active: true, name: place_name.chomp)
+   
 
     place_selected
   end
